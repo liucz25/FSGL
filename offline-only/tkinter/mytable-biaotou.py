2356ae5d8b369cbdee59569c66628504ef7b0610
liucz25 <liuchangzheng25@163.com>
2021-05-02 22:32:37 +0800

    添加表头完成，代码不优雅，需要重构

--- offline-only/tkinter/mytable-biaotou.py
+++ offline-only/tkinter/mytable-biaotou.py
@@ -2,10 +2,12 @@ from tkinter import *
 from pickleTools import psave,pload
 
 class MyTable(Frame):
-    def __init__(self,value,total):
+    def __init__(self,value,total,headlie,headhang):
         Frame.__init__(self)
         self.value = value
         self.total_lie=total
+        self.headlie=headlie
+        self.headhang=headhang
         self.entryWidth=10
 
         self.data=[]
@@ -21,6 +23,8 @@ class MyTable(Frame):
         self.textvar=[]
         self.var_total_lie=[]
         self.var_total_one_hang=[]
+        self.var_head_hang=[]
+        self.var_head_lie=[]
         self.var_total_toal=DoubleVar()
         self.cow = len(self.value) + 1         #5   4人
         self.colomn = len(self.value[0]) + 1   #3   2项目
@@ -47,10 +51,10 @@ class MyTable(Frame):
             self.total_total+=v
 
     def entry_init(self):
-        for i in range(len(self.value) + 1):
+        for i in range(len(self.value) + 2):
             var = []
             text = []
-            for j in range(len(self.value[0]) + 1):
+            for j in range(len(self.value[0]) + 2):
                 text.append(DoubleVar())
                 var.append(Entry(root))
             self.textvar.append(text)
@@ -61,35 +65,57 @@ class MyTable(Frame):
 
         for i in range(len(self.total_lie)):
             self.var_total_lie.append(DoubleVar())
+
+        for i in range(len(self.headhang)):
+            self.var_head_hang.append(StringVar())
+        for i in range(len(self.headlie)):
+            self.var_head_lie.append(StringVar())
         # print(textvar,entry)
         # return entry, textvar
 
 
     def entry_add(self):
-        cow = self.cow
-        colomn = self.colomn
+        cow = self.cow+1
+        colomn = self.colomn+1
         for i in range(cow):
             for j in range(colomn):
                 # print(i,j)
-                if i < (cow - 1) and j < (colomn - 1):
-                    self.textvar[i][j].set(format( self.value[i][j],'.0f'))
-                    self.entry[i][j]["textvariable"] = self.textvar[i][j]
+                if i==0: #and j<=(colomn):
+                    # print(i, j)
+                    self.var_head_lie[j].set(self.headlie[j])
+                    self.entry[i][j]["textvariable"]=self.var_head_lie[j]
+                    self.entry[i][j]["width"]=self.entryWidth
+                    self.entry[i][j].grid(row=i,column=j)
+                    self.entry[i][j]["state"] = 'disabled'
+
+                elif j==0 and i>0 and i<cow:
+                    # print(i,j)
+                    self.var_head_hang[i-1].set(self.headhang[i-1])
+                    self.entry[i][j]["textvariable"]=self.var_head_hang[i-1]
+                    self.entry[i][j]["width"]=self.entryWidth
+                    self.entry[i][j].grid(row=i,column=j)
+                    self.entry[i][j]["state"] = 'disabled'
+
+                elif i > 0 and i < (cow - 1) and j > 0 and j < (colomn - 1):
+                    print(i,j)
+                    self.textvar[i-1][j-1].set(format( self.value[i-1][j-1],'.0f'))
+                    self.entry[i][j]["textvariable"] = self.textvar[i-1][j-1]
                     self.entry[i][j]["width"]=self.entryWidth
                     self.entry[i][j].grid(row=i, column=j)
                     self.entry[i][j].bind("<KeyRelease>", self.reload)  # 需要把是哪个单元格传入，，，或者直接更新全局计算
 
-                elif i < (cow - 1) and j == (colomn - 1):
+                elif i>0 and i < (cow - 1) and j == (colomn - 1):
                     # pass
-                    self.var_total_one_hang[i].set(format( self.total_one_hang[i],'.2f'))
-                    self.entry[i][j]["textvariable"] = self.var_total_one_hang[i]
+                    self.var_total_one_hang[i-1].set(format( self.total_one_hang[i-1],'.2f'))
+                    self.entry[i][j]["textvariable"] = self.var_total_one_hang[i-1]
                     self.entry[i][j]["width"] = self.entryWidth
                     self.entry[i][j]["state"] = 'disabled'
                     self.entry[i][j].grid(row=i, column=j)
                 elif i == (cow - 1) and j < (colomn - 1):
-                    self.var_total_lie[j].set(format( self.total_lie[j],'.2f'))
+                    self.var_total_lie[j-1].set(format( self.total_lie[j-1],'.2f'))
                     # self.entry[i][j]["state"] = 'disabled'
 
-                    self.entry[i][j]["textvariable"] = self.var_total_lie[j]
+                    self.entry[i][j]["textvariable"] = self.var_total_lie[j-1]
                     self.entry[i][j]["width"] = self.entryWidth
                     self.entry[i][j].grid(row=i, column=j)
                     self.entry[i][j].bind("<KeyRelease>", self.reload)
@@ -102,9 +128,9 @@ class MyTable(Frame):
         # row = len(self.value)
         # column=len(self.value[0])
         self.btnLoad = Button( text="加载", command=self.cload)
-        self.btnLoad.grid(row=cow + 2, column=1)
+        self.btnLoad.grid(row=cow + 3, column=1)
         self.btnSave = Button( text="保存", command=self.csave)
-        self.btnSave.grid(row=cow + 2, column=2)
+        self.btnSave.grid(row=cow + 3, column=2)
 
     def fenpei(self):
 
@@ -192,9 +218,11 @@ class MyTable(Frame):
 if __name__ == "__main__":
     root = Tk()
     # column 默认值是 0
-    value = [[1, 2,2], [1, 1,2], [2, 3,1], [4, 0,3]]
+    value = [[1, 2,2], [1, 1,2], [2, 3,1], [4, 0,3],[3,5,7]]
     total_lie = [888, 600,89]
-    app = MyTable(value,total_lie)
+    headlie=["姓名","项目1","项目2","项目3","总和"]
+    headhang=["张三","李四","王五","赵柳","找齐","金额"]
+    app = MyTable(value,total_lie,headlie,headhang)
     # app.t1()
     app.t2()
     app.test()
